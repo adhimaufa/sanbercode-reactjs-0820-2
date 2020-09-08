dbcb61716622b8aba4d7421b70e79ff30e235826
adhimaufa <adhim.aufa@gmail.com>
2020-09-08 07:52:35 +0700

    update tugas 11

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import InputForm from './Tugas-9/inputForm';
 import TableForm from './Tugas-10/tableForm';
+import Timer from './Tugas-11/counterDouble';
 import './App.css';
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
       <InputForm />
       <br/>
       <TableForm />
+      <br/>
+      <Timer/>
     </>   
   );
 }
